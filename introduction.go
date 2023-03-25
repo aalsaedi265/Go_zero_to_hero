c2bcc702371d0842ae28a68bacd0a0c4f0ed6e4d
aalsaedi265 <aalsaedi265@gmail.com>
2023-03-25 09:49:08 -0500

    first

---
+++ introduction.go
@@ -0,0 +1,17 @@
+package main //main or module to execute teh script
+// go run file 
+
+import "fmt" //prints it to the terminal
+
+// function to runt the code main does not need to be called when activated
+func main() {
+    fmt.Println("The World!")
+    
+}
+/*
+Dirty deeds done direty cheep belongs to the best
+jojo vilian Funny Valentine , but
+wounder of u is a close
+second same as for
+Kars 
+*/
