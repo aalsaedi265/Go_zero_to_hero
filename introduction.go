2fb9765b49b666bbf41f6145b0e157ef24ab73f0
aalsaedi265 <aalsaedi265@gmail.com>
2023-03-26 19:15:33 -0500

    learning continues

--- introduction.go
+++ introduction.go
@@ -1,12 +1,22 @@
 package main //main or module to execute teh script
 // go run file 
 
-import "fmt" //prints it to the terminal
+import ("fmt"
+// "math"
+// "strconv" //conversts numbers or anthing to a string and reverse is true
+)
 
 // function to runt the code main does not need to be called when activated
 func main() {
-    fmt.Println("The World!")
-    
+
+   jouskai :="Shining Diamond"
+// rage use int32 data type
+  for i, el := range jouskai {// range is enumerator  _ used as palce holder for novarable
+    //wiout string i would print bite number amount and not character in jouskai
+    fmt.Println(i, string(el)) // index, letter
+  }
+ 
+  
 }
 /*
 Dirty deeds done direty cheep belongs to the best
@@ -15,3 +25,4 @@ wounder of u is a close
 second same as for
 Kars 
 */
+
